b2aba26e1119aaec0bb476a1bf7efd2f03d83485
Ahmer <siddiquiahmer970@gmail.com>
2023-03-11 15:54:29 -0500

    learn about props ,isrequired , defalutprops and props-type in detail

---
+++ Understanding Props and PropTypes in React/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Navbar from "./Components/Navbar";
+
+function App() {
+  return (
+    <>
+      {/* <Navbar title="TextUtils" aboutText="About TextUtils"/> */}
+      <Navbar title="TextUtils"/>
+    </>
+  );
+}
+
+export default App;
